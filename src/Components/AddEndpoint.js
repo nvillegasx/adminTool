143a4b6eeb922e0818d8d9858b20999230cb741a
nvillegasx <nvillegasx@gmail.com>
2019-04-04 10:54:59 -0700

    changes to dynamically add body params

--- src/Components/AddEndpoint.js
+++ src/Components/AddEndpoint.js
@@ -2,6 +2,78 @@ import React, { Component } from 'react'
 import './AddEndpoint.css'
 
 class AddEndpoint extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            bodyParamRows: []
+        }
+        // this.state.bodyParamRows[0] =                 
+        // <div className="row">
+        //     <div className="col-sm">
+        //         <input type="text"></input>
+        //     </div>
+        //     <div className="col-sm">
+        //         <input type="text"></input>
+        //     </div>
+        //     <div className="col-sm">
+        //         <div className="row">
+        //             <div className="col-sm">
+        //                 <input type="radio" name="bodyRequired" value="yes"></input>
+        //                 Yes
+        //             </div>
+        //             <div className="col-sm">
+        //                 <input type="radio" name="bodyRequired" value="no"></input>
+        //                 No
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div className="col-sm">
+        //         <input type="text"></input>
+        //     </div>
+        // </div>
+    }
+
+    addBodyParam = () =>
+    {
+        
+        let newRow =
+            <div className="row">
+                <div className="col-sm">
+                    <input type="text"></input>
+                </div>
+                <div className="col-sm">
+                    <input type="text"></input>
+                </div>
+                <div className="col-sm">
+                    <div className="row">
+                        <div className="col-sm">
+                            <input type="radio" name="bodyRequired" value="yes"></input>
+                            Yes
+                        </div>
+                        <div className="col-sm">
+                            <input type="radio" name="bodyRequired" value="no"></input>
+                            No
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm">
+                    <input type="text"></input>
+                </div>
+            </div>
+        
+        this.setState({ bodyParamRows: this.state.bodyParamRows.push(newRow)})
+        // this.state.bodyParamRows.push(newRow)
+        
+        // table.appendChild(divRow)
+        console.log(this.state.bodyParamRows)
+    }
+
+    componentWillUpdate = () => {
+        
+    }
+    displayRows = () =>{
+        return this.state.bodyParamRows
+    }
   render() {
     return (
       <div className="w-50 p-3">
@@ -24,8 +96,9 @@ class AddEndpoint extends Component {
                     <option>DELETE</option>
                 </select>
             </div>
-            <div className="form-group">
-            <label>Body Parameters</label>
+            {/* bodyParamField */}
+            <div className="form-group" id="BP" ref="BP">
+                <label>Body Parameters</label>
                 <div className="row">
                     <div className="col-sm">
                         <p>Name</p>
@@ -48,14 +121,71 @@ class AddEndpoint extends Component {
                         <input type="text"></input>
                     </div>
                     <div className="col-sm">
-                    {/* FIX THIS TODO */}
-                        {/* <div>
-                            <input type="radio" name="required" value="yes">Yes</input>
-                        </div> */}
-                        {/* <div>
-                            <input type="radio" name="required" value="no">No</input>
-                        </div> */}
+                        <div className="row">
+                            <div className="col-sm">
+                                <input type="radio" name="bodyRequired" value="yes"></input>
+                                Yes
+                            </div>
+                            <div className="col-sm">
+                                <input type="radio" name="bodyRequired" value="no"></input>
+                                No
+                            </div>
+                        </div>
                     </div>
+                    <div className="col-sm">
+                        <input type="text"></input>
+                    </div>
+                </div>
+                {this.displayRows()}
+                <div className="row">
+                    <div className="col-sm"></div>
+                    <div className="col-sm"><button onClick={() => this.addBodyParam()}>Add Parameter</button></div>
+                    <div className="col-sm"></div>
+                </div>
+            </div>
+            <div className="form-group">
+                <label>Path Parameters</label>
+                <div className="row">
+                    <div className="col-sm">
+                        <p>Name</p>
+                    </div>
+                    <div className="col-sm">
+                        Type
+                    </div>
+                    <div className="col-sm">
+                        Required
+                    </div>
+                    <div className="col-sm">
+                        Description
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <input type="text"></input>
+                    </div>
+                    <div className="col-sm">
+                        <input type="text"></input>
+                    </div>
+                    <div className="col-sm">
+                        <div className="row">
+                            <div className="col-sm">
+                                <input type="radio" name="pathRequired" value="yes"></input>
+                                Yes
+                            </div>
+                            <div className="col-sm">
+                                <input type="radio" name="pathRequired" value="no"></input>
+                                No
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <input type="text"></input>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm"></div>
+                    <div className="col-sm"><button>Add Parameter</button></div>
+                    <div className="col-sm"></div>
                 </div>
             </div>
         </form>
