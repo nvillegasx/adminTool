c67a0a2dcfa14a69ce531ae1ecfd7a3d7ecab950
nvillegasx <nvillegasx@gmail.com>
2019-04-08 09:36:57 -0700

    changes to save the information of each body and path parameter

--- src/Components/AddEndpoint.js
+++ src/Components/AddEndpoint.js
@@ -8,41 +8,93 @@ class AddEndpoint extends Component {
         this.state ={
             bodyParamRows: [],
             pathParamRows: [],
+            bodyParams:[],
+            pathParams:[],
             numBodyParam: 0,
             numPathParam: 0
         }
 
+    }
+    setBodyParamName=(idx, e)=>{
+        // use the idx to set the array element to the needed values
+        if(this.state.bodyParams == [] || this.state.bodyParams[idx] == undefined )
+        {
+            this.state.bodyParams[idx] = {name: e.target.value}
+        }
+        else{
+            this.state.bodyParams[idx].name = e.target.value
+        }
+
+        console.log(this.state.bodyParams[idx])
+    }
+
+    setBodyParamType=(idx, e) => {
+        if(this.state.bodyParams == [] || this.state.bodyParams[idx] == undefined )
+        {
+            this.state.bodyParams[idx] = {type: e.target.value}
+        }
+        else{
+            this.state.bodyParams[idx].type = e.target.value
+        }
+
+        console.log(this.state.bodyParams[idx])
+
+    }
+
+    setBodyParamReq=(idx, e) => {
+        if(this.state.bodyParams == [] || this.state.bodyParams[idx] == undefined )
+        {
+            this.state.bodyParams[idx] = {required: e.target.value}
+        }
+        else{
+            this.state.bodyParams[idx].required = e.target.value
+        }
+
+        console.log(this.state.bodyParams[idx])
+    }
+
+    setBodyParamDesc=(idx, e) => {
+        if(this.state.bodyParams == [] || this.state.bodyParams[idx] == undefined )
+        {
+            this.state.bodyParams[idx] = {description: e.target.value}
+        }
+        else{
+            this.state.bodyParams[idx].description = e.target.value
+        }
+
+        console.log(this.state.bodyParams[idx])
     }
 
 
 
     addBodyParamRow=()=>{
         let newName = "b" + this.state.numBodyParam
+        let paramIdx = this.state.numBodyParam
         var  row = this.state.bodyParamRows
         let newrow =
         <div className="form-group">
 
         <div className="row">
             <div className="col-sm">
-                <input type="text"></input>
+                <input type="text" onChange={ (e) => this.setBodyParamName(paramIdx, e)}></input>
             </div>
             <div className="col-sm">
-                <input type="text"></input>
+                <input type="text" onChange={ (e) => this.setBodyParamType(paramIdx, e)}></input>
             </div>
             <div className="col-sm">
                 <div className="row">
                     <div className="col-sm">
-                        <input type="radio" name={newName} value="yes"></input>
+                        <input type="radio" name={newName} value="yes" onClick={(e) => this.setBodyParamReq(paramIdx, e)}></input>
                         Yes
                     </div>
                     <div className="col-sm">
-                        <input type="radio" name={newName} value="no"></input>
+                        <input type="radio" name={newName} value="no" onClick={ (e) => this.setBodyParamReq(paramIdx, e)}></input>
                         No
                     </div>
                 </div>
             </div>
             <div className="col-sm">
-                <input type="text"></input>
+                <input type="text" onChange={ (e) => this.setBodyParamDesc(paramIdx, e) }></input>
             </div>
         </div>
         </div>
@@ -54,33 +106,85 @@ class AddEndpoint extends Component {
         row.push(newrow)
         this.setState({ bodyParamRows: row})
     }
+
+    setPathParamName=(idx, e)=>{
+        // use the idx to set the array element to the needed values
+        if(this.state.pathParams == [] || this.state.pathParams[idx] == undefined )
+        {
+            this.state.pathParams[idx] = {name: e.target.value}
+        }
+        else{
+            this.state.pathParams[idx].name = e.target.value
+        }
+
+        console.log(this.state.pathParams[idx])
+    }
+
+    setPathParamType=(idx, e) => {
+        if(this.state.pathParams == [] || this.state.pathParams[idx] == undefined )
+        {
+            this.state.pathParams[idx] = {type: e.target.value}
+        }
+        else{
+            this.state.pathParams[idx].type = e.target.value
+        }
+
+        console.log(this.state.pathParams[idx])
+
+    }
+
+    setPathParamReq=(idx, e) => {
+        if(this.state.pathParams == [] || this.state.pathParams[idx] == undefined )
+        {
+            this.state.pathParams[idx] = {required: e.target.value}
+        }
+        else{
+            this.state.pathParams[idx].required = e.target.value
+        }
+
+        console.log(this.state.pathParams[idx])
+    }
+
+    setPathParamDesc=(idx, e) => {
+        if(this.state.pathParams == [] || this.state.pathParams[idx] == undefined )
+        {
+            this.state.pathParams[idx] = {description: e.target.value}
+        }
+        else{
+            this.state.pathParams[idx].description = e.target.value
+        }
+
+        console.log(this.state.pathParams[idx])
+    }
     addRow=()=>{
+
         var  row = this.state.pathParamRows
         let newName = "p" + this.state.numPathParam
+        let paramIdx = this.state.numPathParam
         let newrow =
         <div className="form-group">
 
         <div className="row">
             <div className="col-sm">
-                <input type="text"></input>
+                <input type="text" onChange={ (e) => this.setPathParamName(paramIdx, e)}></input>
             </div>
             <div className="col-sm">
-                <input type="text"></input>
+                <input type="text" onChange={ (e) => this.setPathParamType(paramIdx, e)}></input>
             </div>
             <div className="col-sm">
                 <div className="row">
                     <div className="col-sm">
-                        <input type="radio" name={newName} value="yes"></input>
+                        <input type="radio" name={newName} value="yes" onClick={(e) => this.setPathParamReq(paramIdx, e)}></input>
                         Yes
                     </div>
                     <div className="col-sm">
-                        <input type="radio" name={newName} value="no"></input>
+                        <input type="radio" name={newName} value="no" onClick={(e) => this.setPathParamReq(paramIdx, e)}></input>
                         No
                     </div>
                 </div>
             </div>
             <div className="col-sm">
-                <input type="text"></input>
+                <input type="text" onChange={ (e) => this.setPathParamDesc(paramIdx, e) }></input>
             </div>
         </div>
         </div>
